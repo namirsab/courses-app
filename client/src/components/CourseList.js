9745011086a2828582813e452014957bb2c99b2c
Namir Sayed-Ahmad <namirsab@gmail.com>
2021-04-21 12:46:22 +0200

    Make CourseListItem component receive props and use them

--- client/src/components/CourseList.js
+++ client/src/components/CourseList.js
@@ -5,11 +5,31 @@ function CourseList() {
   return (
     <div>
       <h2>CourseList</h2>
-      <CourseListItem />
-      <CourseListItem />
-      <CourseListItem />
-      <CourseListItem />
-      <CourseListItem />
+      <CourseListItem
+        courseIndex="1"
+        courseName="React Router is amazing"
+        courseLocation="Remote"
+      />
+      <CourseListItem
+        courseIndex="2"
+        courseName="NodeJS for beginners"
+        courseLocation="Hamburg"
+      />
+      <CourseListItem
+        courseIndex="3"
+        courseName="Express: is it so fast?"
+        courseLocation="Cologne"
+      />
+      <CourseListItem
+        courseIndex="4"
+        courseName="React Custom Hooks: wtf"
+        courseLocation="Munich"
+      />
+      <CourseListItem
+        courseIndex="5"
+        courseName="Adobe XD by Sascha Lerner"
+        courseLocation="Hamburg"
+      />
     </div>
   );
 }
--- client/src/components/CourseListItem.js
+++ client/src/components/CourseListItem.js
@@ -1,7 +1,16 @@
 import "./CourseListItem.css";
 
-function CourseListItem() {
-  return <div>Course List Item </div>;
+function CourseListItem({ courseIndex, courseName, courseLocation }) {
+  return (
+    <div className="CourseListItem">
+      <div className="CourseListItem__index">{courseIndex}</div>
+      <div className="CourseListItem__details">
+        <div className="CourseListItem__name">{courseName}</div>
+        <div className="CourseListItem__location">{courseLocation}</div>
+      </div>
+      <button className="CourseListItem__button">{"->"}</button>
+    </div>
+  );
 }
 
 export default CourseListItem;
