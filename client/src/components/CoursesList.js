f7bcce44e8562e5a31a4c406d4082f64cb48b5ea
Namir Sayed-Ahmad <namirsab@gmail.com>
2021-04-21 09:20:13 +0200

    Implement dummy server

--- client/src/components/CoursesList.js
+++ client/src/components/CoursesList.js
@@ -1,104 +1,54 @@
+import { useEffect, useState } from "react";
 import CourseListItem from "./CourseListItem";
 import "./CoursesList.css";
 
 function CoursesList() {
-  const courses = [
-    {
-      _id: "1",
-      name: "Advanced React",
-      location: "Remote",
-    },
-    {
-      _id: "2",
-      name: "CSS for Beginners",
-      location: "Hamburg",
-    },
-    {
-      _id: "3",
-      name: "What the hell is JavaScript?",
-      location: "Köln",
-    },
-    {
-      _id: "4",
-      name: "MongoDB: storing data the easy way",
-      location: "Remote",
-    },
-    {
-      _id: "5",
-      name: "NodeJS on steroids",
-      location: "Remote",
-    },
-    {
-      _id: "6",
-      name: "NodeJS on steroids",
-      location: "Remote",
-    },
-    {
-      _id: "7",
-      name: "NodeJS on steroids",
-      location: "Remote",
-    },
-    {
-      _id: "8",
-      name: "NodeJS on steroids",
-      location: "Remote",
-    },
-    {
-      _id: "9",
-      name: "NodeJS on steroids",
-      location: "Remote",
-    },
-    {
-      _id: "10",
-      name: "NodeJS on steroids",
-      location: "Remote",
-    },
-    {
-      _id: "11",
-      name: "NodeJS on steroids",
-      location: "Remote",
-    },
-    {
-      _id: "12",
-      name: "NodeJS on steroids",
-      location: "Remote",
-    },
-    {
-      _id: "13",
-      name: "NodeJS on steroids",
-      location: "Remote",
-    },
-    {
-      _id: "14",
-      name: "NodeJS on steroids",
-      location: "Remote",
-    },
-  ];
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("http://localhost:4000/api/courses")
+      .then((res) => res.json())
+      .then((data) => {
+        setCourses(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        setError(true);
+      });
+  }, []);
 
   function renderCourses() {
-    return courses.map((course, index) => {
-      return (
-        <CourseListItem
-          id={course._id}
-          index={index + 1}
-          courseName={course.name}
-          courseLocation={course.location}
-          key={course._id}
-          onClick={handleCourseClick}
-        />
-      );
-    });
+    if (loading) {
+      return "Loading...";
+    } else if (error) {
+      return "Error!";
+    } else if (courses.length === 0) {
+      return "No available courses";
+    } else {
+      return courses.map((course, index) => {
+        return (
+          <CourseListItem
+            id={course._id}
+            index={index + 1}
+            courseName={course.name}
+            courseLocation={course.location}
+            key={course._id}
+            onClick={handleCourseClick}
+          />
+        );
+      });
+    }
   }
 
   function handleCourseClick(courseId) {
     console.log(`Course with id ${courseId} clicked`);
   }
 
-  return (
-    <div className="CoursesList">
-      {courses.length > 0 ? renderCourses() : "No courses available"}
-    </div>
-  );
+  return <div className="CoursesList">{renderCourses()}</div>;
 }
 
 export default CoursesList;
--- server.js
+++ server.js
@@ -11,8 +11,85 @@ app.get("/", (req, res) => {
   res.status(200).send("Hello World");
 });
 
+const courses = [
+  {
+    _id: "1",
+    name: "Advanced React",
+    location: "Remote",
+  },
+  {
+    _id: "2",
+    name: "CSS for Beginners",
+    location: "Hamburg",
+  },
+  {
+    _id: "3",
+    name: "What the hell is JavaScript?",
+    location: "Köln",
+  },
+  {
+    _id: "4",
+    name: "MongoDB: storing data the easy way",
+    location: "Remote",
+  },
+  {
+    _id: "5",
+    name: "NodeJS on steroids",
+    location: "Remote",
+  },
+  {
+    _id: "6",
+    name: "NodeJS on steroids",
+    location: "Remote",
+  },
+  {
+    _id: "7",
+    name: "NodeJS on steroids",
+    location: "Remote",
+  },
+  {
+    _id: "8",
+    name: "NodeJS on steroids",
+    location: "Remote",
+  },
+  {
+    _id: "9",
+    name: "NodeJS on steroids",
+    location: "Remote",
+  },
+  {
+    _id: "10",
+    name: "NodeJS on steroids",
+    location: "Remote",
+  },
+  {
+    _id: "11",
+    name: "NodeJS on steroids",
+    location: "Remote",
+  },
+  {
+    _id: "12",
+    name: "NodeJS on steroids",
+    location: "Remote",
+  },
+  {
+    _id: "13",
+    name: "NodeJS on steroids",
+    location: "Remote",
+  },
+  {
+    _id: "14",
+    name: "NodeJS on steroids",
+    location: "Remote",
+  },
+];
+
+app.get("/api/courses", (req, res) => {
+  res.status(200).json(courses);
+});
+
 const { PORT } = process.env;
 
 app.listen(PORT, () => {
-  console.log("listening");
+  console.log(`listening in port ${PORT}`);
 });
