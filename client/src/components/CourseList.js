f026cfeb7cfbf2acf81bb3845da5086426346ed2
Namir Sayed-Ahmad <namirsab@gmail.com>
2021-04-21 12:55:46 +0200

    Make the CourseList use fake data to render the CourseList Items

--- client/src/components/CourseList.js
+++ client/src/components/CourseList.js
@@ -2,34 +2,68 @@ import "./CourseList.css";
 import CourseListItem from "./CourseListItem";
 
 function CourseList() {
+  const courses = [
+    {
+      _id: "a",
+      name: "React Router is amazing",
+      location: "Remote",
+    },
+    {
+      _id: "b",
+      name: "NodeJS for beginners",
+      location: "Hamburg",
+    },
+    {
+      _id: "c",
+      name: "Express: is it so fast",
+      location: "Cologne",
+    },
+    {
+      _id: "d",
+      name: "React Custom Hooks: wtf",
+      location: "Munich",
+    },
+    {
+      _id: "e",
+      name: "Adobe XD by Sascha Lerner",
+      location: "Hamburg",
+    },
+    {
+      _id: "f",
+      name: "Adobe XD by Sascha Lerner",
+      location: "Hamburg",
+    },
+    {
+      _id: "g",
+      name: "Adobe XD by Sascha Lerner",
+      location: "Hamburg",
+    },
+    {
+      _id: "h",
+      name: "Adobe XD by Sascha Lerner",
+      location: "Hamburg",
+    },
+  ];
+
+  function renderCourses() {
+    const coursesComponents = courses.map((course, index) => {
+      return (
+        <CourseListItem
+          key={course._id}
+          courseIndex={index + 1}
+          courseName={course.name}
+          courseLocation={course.location}
+        />
+      );
+    });
+
+    return coursesComponents;
+  }
+
   return (
     <div>
       <h2>CourseList</h2>
-      <CourseListItem
-        courseIndex="1"
-        courseName="React Router is amazing"
-        courseLocation="Remote"
-      />
-      <CourseListItem
-        courseIndex="2"
-        courseName="NodeJS for beginners"
-        courseLocation="Hamburg"
-      />
-      <CourseListItem
-        courseIndex="3"
-        courseName="Express: is it so fast?"
-        courseLocation="Cologne"
-      />
-      <CourseListItem
-        courseIndex="4"
-        courseName="React Custom Hooks: wtf"
-        courseLocation="Munich"
-      />
-      <CourseListItem
-        courseIndex="5"
-        courseName="Adobe XD by Sascha Lerner"
-        courseLocation="Hamburg"
-      />
+      {renderCourses()}
     </div>
   );
 }
