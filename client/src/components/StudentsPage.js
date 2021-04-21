b6552f5b8525ecead42356e10b464ddf6547fc08
Namir Sayed-Ahmad <namirsab@gmail.com>
2021-04-21 09:28:56 +0200

    Fix layout. Add dummy content to see scrolling working

--- client/src/components/StudentsPage.js
+++ client/src/components/StudentsPage.js
@@ -1,5 +1,74 @@
 function StudentsPage() {
-  return <div>Students Page</div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus, eaque
+      et explicabo perspiciatis, rerum veniam magnam quisquam aliquam ullam, ea
+      ad voluptatum a ipsa molestias nihil. Debitis in voluptas soluta.
+      Asperiores libero sint similique autem dolore, beatae dolorum. Quaerat,
+      placeat? Rerum dolore doloribus, fugiat optio adipisci unde cumque velit
+      illum nihil impedit maxime nobis corporis dignissimos fugit, deleniti eos
+      voluptate! Iusto provident dolores quidem numquam nulla accusantium
+      placeat fugiat consequuntur nihil in voluptates impedit aperiam soluta
+      reprehenderit, quia magni aspernatur odit illo quae! Rerum rem
+      perspiciatis dicta nobis, veniam consequatur? Ut, asperiores ex maiores
+      beatae fugit maxime libero accusantium illo nostrum in? Suscipit excepturi
+      nesciunt sit praesentium eligendi sequi aspernatur perferendis error
+      expedita libero ipsam alias nihil, fugit repellendus minus! Atque, animi!
+      Recusandae, provident dolorem! Est quibusdam, at delectus dolor a ab amet
+      sunt quasi in, facere non incidunt debitis nam eos! Dolorum unde iste
+      fugit quo est deleniti reprehenderit? Consequuntur reiciendis, molestias
+      quasi minus sint mollitia delectus sequi dolore, eaque nobis enim
+      voluptate beatae incidunt quibusdam ea id ab. Dolorem modi voluptatibus
+      repudiandae id expedita sequi, quis consectetur quidem. Temporibus neque
+      itaque animi ducimus officia, necessitatibus consequatur error, fugit ea
+      molestias aliquam culpa aspernatur nesciunt sunt. Provident maxime rerum
+      nemo incidunt! Mollitia at aliquam, numquam nostrum odio vero dicta?
+      Labore, deserunt? Beatae ab aliquam aut nesciunt corrupti nihil aperiam!
+      Ipsa iure voluptas ex porro pariatur excepturi, explicabo culpa ipsam
+      ipsum, sapiente saepe, dolor aspernatur consequuntur quis nobis illo
+      aliquam! Doloribus ab blanditiis cum voluptatem, similique laboriosam,
+      perferendis vel rerum, dignissimos eligendi doloremque! Aliquam
+      dignissimos accusamus sapiente dolorum ad, nisi adipisci esse laborum
+      maiores necessitatibus mollitia asperiores voluptate! Eius, quibusdam.
+      Veniam, fugit corrupti incidunt reiciendis, ducimus tempore similique,
+      facilis nulla earum laudantium cupiditate sapiente dolorem ea molestias
+      quidem ullam culpa consectetur totam. Enim ab nihil sapiente facilis fugit
+      consectetur maxime! Nisi distinctio corporis blanditiis quaerat neque
+      nobis accusamus facere veritatis, laboriosam tenetur corrupti
+      necessitatibus qui maxime provident, fugit ex eligendi aliquid recusandae.
+      Magnam, voluptate nesciunt. Amet consequuntur architecto dolore deleniti!
+      Iusto beatae corporis natus ipsa labore, praesentium odio asperiores
+      provident, quidem libero facere porro laudantium dignissimos qui
+      perferendis odit dolores dolorum molestiae saepe debitis quam. In at
+      voluptate velit nemo. Suscipit aperiam voluptates consequuntur, ab
+      eligendi tempore inventore, natus repellat totam nulla rem modi beatae,
+      veniam ratione nobis numquam mollitia culpa ea hic quibusdam? Eligendi
+      voluptate fugiat est alias neque? Laboriosam dolorum exercitationem
+      repellendus optio explicabo modi doloremque mollitia dicta delectus
+      provident tenetur ea dolorem quam non assumenda, voluptatem eos inventore
+      consequuntur repellat aspernatur enim error hic unde necessitatibus.
+      Adipisci! Praesentium, voluptates expedita? Quod laboriosam natus sapiente
+      eaque tenetur, enim fugit officia praesentium ducimus rem amet eligendi
+      saepe mollitia porro laborum esse distinctio accusantium dolor neque nisi.
+      Eaque, ducimus reiciendis. Nulla dolore voluptatum aliquid, repudiandae
+      libero debitis accusantium. Quibusdam velit ab consequuntur, nobis quod
+      deleniti cumque iusto illo, labore officiis molestias minus. Facilis
+      quaerat sint, consectetur ducimus vel illo commodi! Atque accusantium
+      omnis architecto eligendi non consequatur ab sit molestiae dolorum, labore
+      maiores natus. Ut incidunt saepe dolor quos repellendus aliquam sint odio.
+      Repellendus omnis laboriosam fugiat modi accusamus quaerat. Totam dolor
+      delectus amet. Consequuntur suscipit ipsam tempore! Perferendis sequi
+      placeat dolorem beatae, voluptas saepe vitae eaque in quam quo tempore
+      incidunt dolorum! Perferendis omnis laudantium quasi, soluta sit
+      reiciendis. Rem, obcaecati recusandae voluptate, sint quas similique
+      voluptas, harum magni praesentium repellat facere ex beatae accusantium
+      nihil natus corporis numquam debitis autem odio ad? Nemo provident
+      voluptate nobis non dicta. Minima provident illum nesciunt maiores soluta
+      quisquam quis, temporibus ipsam saepe distinctio vero. Perspiciatis
+      molestias quia error esse possimus voluptatem quam est saepe, repellendus
+      eum omnis doloremque facere. Cupiditate, natus?
+    </div>
+  );
 }
 
 export default StudentsPage;
